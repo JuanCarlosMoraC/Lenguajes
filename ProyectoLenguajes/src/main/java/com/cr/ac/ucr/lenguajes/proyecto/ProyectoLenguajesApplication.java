aa2b4370431be40e693dde4369d00a1775913ae6
Juan Carlos <jcarlosmc1994@gmail.com>
2017-05-09 14:43:11 -0600

    Proyecto Uno
    Lenguajes J2F Shop

---
+++ ProyectoLenguajes/src/main/java/com/cr/ac/ucr/lenguajes/proyecto/ProyectoLenguajesApplication.java
@@ -0,0 +1,12 @@
+package com.cr.ac.ucr.lenguajes.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLenguajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLenguajesApplication.class, args);
+	}
+}
